61cc31763bac86738aa97c5acfae99c87c1378e2
Lila AKSIL <lilaaksil@gmail.com>
2020-01-16 14:03:41 +0100

    initial commit.
    Vanilla Spring Boot project

---
+++ src/main/java/fr/valarep/projetlila/ProjetlilaApplication.java
@@ -0,0 +1,13 @@
+package fr.valarep.projetlila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetlilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetlilaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/valarep/projetlila/ProjetlilaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.valarep.projetlila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetlilaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
